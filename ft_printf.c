752e84b70c1bce3707c740e27be31737124ed068
sajeon <1127lse@gmail.com>
2021-02-20 19:00:58 +0900

    ft_printf.c blueprint

--- ft_printf.c
+++ ft_printf.c
@@ -6,20 +6,67 @@
 /*   By: sajeon <sajeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 21:50:39 by sajeon            #+#    #+#             */
-/*   Updated: 2021/02/20 15:27:52 by sajeon           ###   ########.fr       */
+/*   Updated: 2021/02/20 19:00:14 by sajeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printf(void)
+t_info	init_info(t_info info)
 {
-	int test = 1;
-	char test_str[40] = "hello";
-	char dest[40];
+	info.zero = 0;
+	info.minus = 0;
+	info.dot = -1;
+	info.star = 0;
+	info.width = 0;
+	info.type = 0;
+	info.char_count = 0;
+}
 
-	printf("%d\n", test);
-	printf("%s\n", test_str);
-	printf("%s\n", ft_memcpy(dest, test_str, 5));
-	return (1);
+int	flag_parse(const char *input, int i, t_info *info, va_list args)
+{
+	while (input[i++])
+	{
+		if (input[i] == '0' && info->width == 0 && info->minus == 0)
+			info->zero = 1;
+		if (input[i] == '-')
+			treat_flag_minus(*info);
+		if (input[i] == '.')
+			i = treat_flag_dot(input, i, info, args);
+		if (input[i] == '*')
+			treat_flag_width(*info, args);
+		if (is_in_type_list(input[i]))
+		{
+			info->type = input[i];
+			put_by_type(*info, args);
+			break;
+		}
+		if (!ft_isdigit(input[i]) && !is_in_type_list(input[i])
+			&& !is_in_flag_list(input[i]))
+			break;
+	}
+	return (i);
+}
+
+int	ft_printf(const char *input, ...)
+{
+	va_list	args;
+	int		i;
+	t_info	info;
+
+	i = 0;
+	va_start(args, input);
+	while (input[i++])
+	{
+		init_info(info);
+		if (input[i] == '%' && input[i + 1])
+			i = flag_parse(input, ++i, &info, args);
+		else
+		{
+			ft_putchar_fd(input[i], 1);
+			info.char_count++;
+		}
+	}
+	va_end(args);
+	return (info.char_count);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,28 @@
 /*   By: sajeon <sajeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 22:02:20 by sajeon            #+#    #+#             */
-/*   Updated: 2021/02/20 16:37:44 by sajeon           ###   ########.fr       */
+/*   Updated: 2021/02/20 17:21:23 by sajeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-#include "libft/libft.h"
 
-int ft_printf(void);
+#include <stdio.h> // **** only for testing ****
+#include "libft/libft.h"
+#include <stdarg.h>
+
+typedef struct s_info
+{
+    int zero;
+    int minus;
+	int dot;
+    int star;
+    int width;
+	int type;
+    int char_count;
+}              t_info;
+
+int	ft_printf(const char *input, ...);
 
 #endif
